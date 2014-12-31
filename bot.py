43ae63fe354588c4e81bf6f4a271bb57aaf72302
technoboy10 <technoboy10@fake.email>
2014-12-30 17:26:57 -0800

    MARKOVBOT

--- bot.py
+++ bot.py
@@ -4,26 +4,33 @@
 
 import socket
 import select
-import random
 import ssl
 import sys
-from jokes import jokes
+import time
+import random
+import urllib
+from pymarkovchain import *
+from bs4 import BeautifulSoup as Soup
 
-if len(sys.argv) != 5:
-    print "Usage: python %s <host> <channel (no #)> [--ssl|--plain] <nick>"
+if len(sys.argv) != 6:
+    print "Usage: python %s <host> <password> <channel> [--ssl|--plain] <nick>"
     exit(0)
 
 HOST = sys.argv[1]
-CHANNEL = "#"+sys.argv[2]
-SSL = sys.argv[3].lower() == '--ssl'
+PASS = sys.argv[2]
+CHANNEL = "#"+sys.argv[3]
+SSL = sys.argv[4].lower() == '--ssl'
 PORT = 6697 if SSL else 6667
 
-NICK = sys.argv[4]
+NICK = sys.argv[5]
 
 plain = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s = ssl.wrap_socket(plain) if SSL else plain
 
 print "Connecting..."
+
+m = MarkovChain('irc2')
+db = open("db.txt", "r").read()
 
 s.connect((HOST, PORT))
 def read_loop(callback):
@@ -43,17 +50,20 @@ def read_loop(callback):
             s.sendall("PART %s :Bye\r\n"%(CHANNEL))
             s.close()
             exit(0)
+        time.sleep(0.5)
 
 print "Registering..."
 
+s.sendall("PASS %s\r\n"%PASS)
 s.sendall("NICK %s\r\n"%(NICK))
-s.sendall("USER %s * * :A joke bot\r\n"%(NICK))
+s.sendall("USER %s * * :tb10\r\n"%(NICK))
 
 
 connected = False
 def got_message(message):
     print message
     global connected # yes, bad Python style. but it works to explain the concept, right?
+    global db # this too :P   
     words = message.split(' ')
     if 'PING' in message:
         s.sendall('PONG\r\n') # it never hurts to do this :)
@@ -63,7 +73,21 @@ def got_message(message):
         connected = True
         s.sendall("JOIN %s\r\n"%(CHANNEL))
         print "Joining..."
-    elif words[1] == 'PRIVMSG' and words[2] == CHANNEL and '!joke' in words[3] and connected:
-        # Someone probably said `!joke`.
-        s.sendall("PRIVMSG %s :"%(CHANNEL) + random.choice(jokes) + "\r\n")
+    elif words[1] == 'PRIVMSG' and words[2] == CHANNEL and connected:
+        # Someone probably said something.
+        if '!markov' in words[3]:
+            m.generateDatabase(db)
+            s.sendall("PRIVMSG %s :"%(CHANNEL) + m.generateString() + "\r\n")
+        #elif '!web' in words[3]:
+         #   db += Soup(urllib.urlopen(words[4]).read()).get_text().encode("utf-8") + " "            
+          #  s.sendall("PRIVMSG %S :"%(CHANNEL) + "k, added %s"%(words[4]) + "\r\n")"""
+        else:
+           # try:
+            db += message.split(CHANNEL + " :")[1]
+            db += " "
+            open('db.txt', 'w').write(db)
+     
+            if (random.choice(list(range(0,10))) == 1):
+                s.sendall("PRIVMSG %s :"%(CHANNEL) + random.choice(["!moose", "!joke"]) + "\r\n")           
+
 read_loop(got_message)
